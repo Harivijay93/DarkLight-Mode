3dc70eb239ef309a8c202ce91120d2bf1ffe917f
Harinath V <140420368+Harivijay93@users.noreply.github.com>
2025-07-28 10:59:09 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const toggleDarkMode =() =>{
         </div>
         <button onClick={toggleDarkMode} className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 h-9 
         lg:w-10 lg:h-10 flex justify-center items-center rounded-full bg-amber-500 text-neutral-950
-        shadow-lg hover:bg-amber-600 transition-colors'>
+        shadow-lg hover:bg-amber-600 transition-colors z-10'>
             <i className={`bx bx-${darkMode ? "sun" : "moon"} text-lg lg:text-xl`}></i>
         </button>
         <Hero /></div>
